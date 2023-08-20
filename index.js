184bea6496e0d14bc0926a2890a6fa47406d7f42
Rasco <xrascoz@gmail.com>
2023-08-20 18:22:37 +0300

    change in cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin: '*'}));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
