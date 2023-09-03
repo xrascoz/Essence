e197c7d36824156058d73c263810ff0d8608f949
Rasco <xrascoz@gmail.com>
2023-09-03 10:00:59 +0300

    change the cors origin to main website

--- index.js
+++ index.js
@@ -18,31 +18,33 @@ cloudinary.config({
 
  
 paypal.configure({
-    'mod': 'sandbox', // sandbox or live
+    'mod': 'sandbox', 
     'client_id': process.env.CLIENT_ID,
     'client_secret': process.env.CLIENT_SECRET
 });
 
-function checkOrigin(req, res, next) {
-    const allowedOrigins = ['https://essenceob.com'];
-    const requestOrigin = req.get('Referer');
-    if (allowedOrigins.includes(requestOrigin)) {
-        return next(); 
-    }
-    return res.status(403).json({ message: 'Access denied.' });
-}
 
+// function checkOrigin(req, res, next) {
+//     const allowedOrigins = ['https://essenceob.com'];
+//     const requestOrigin = req.get('Referer');
+//     const requestSecret = req.get('Secret-Key'); 
 
-app.use(checkOrigin);
+//     if (allowedOrigins.includes(requestOrigin) && requestSecret === process.env.SECRET_KEY) {
+//         return next(); 
+//     }
+//     return res.status(403).json({ message: 'Access denied.' });
+// }
+
+// app.use(checkOrigin);
 
 app.use(express.json());
-app.use(cors({ origin: '*' }));
-
+app.use(cors({ origin: 'https://essenceob.com' })); 
+// app.use(cors({ origin: '*' }));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 mongoose.connect(`mongodb+srv://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@cluster0.x0sffwq.mongodb.net/essence?retryWrites=true&w=majority`).then(() => {
-    console.log("Api connected");
+    console.log("Api connected");g
 }).catch(err => {
     console.log(err);
 })
@@ -50,12 +52,9 @@ mongoose.connect(`mongodb+srv://${process.env.API_USERNAME}:${process.env.API_PA
 app.get("/", (req, res) => {
     res.send("Rasco New")
 })
-app.use("/api", router)
 
+app.use("/api", router);
 
-
-
-
-app.listen(PORT, (req, res) => {
-    console.log("server listening on port");
-})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
