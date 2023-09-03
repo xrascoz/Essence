46612c0438b21e867443afcdc1ee1c2b21ba6808
Rasco <xrascoz@gmail.com>
2023-09-03 10:15:30 +0300

    change the email sender to main email website

--- index.js
+++ index.js
@@ -7,20 +7,11 @@ const cors = require('cors');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const cloudinary = require("cloudinary").v2
-const paypal = require("paypal-rest-sdk")
-
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET,
-});
-
- 
-paypal.configure({
-    'mod': 'sandbox', 
-    'client_id': process.env.CLIENT_ID,
-    'client_secret': process.env.CLIENT_SECRET
 });
 
 
@@ -38,13 +29,13 @@ paypal.configure({
 // app.use(checkOrigin);
 
 app.use(express.json());
-app.use(cors({ origin: 'https://essenceob.com' })); 
-// app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: 'https://essenceob.com' })); 
+app.use(cors({ origin: '*' }));
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 mongoose.connect(`mongodb+srv://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@cluster0.x0sffwq.mongodb.net/essence?retryWrites=true&w=majority`).then(() => {
-    console.log("Api connected");g
+    console.log("Api connected");
 }).catch(err => {
     console.log(err);
 })
